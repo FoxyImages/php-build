f2fafaa39e5bffebe6bd50e9a6a7e2f535ada51f
Tomas Jacik <tomas@jacik.cz>
2019-08-15 14:29:35 +0200

    Fixed PHP version comparison

--- image/packages.sh
+++ image/packages.sh
@@ -41,7 +41,8 @@ $minimal_apt_get_install \
 	php-mongodb \
 	php-redis
 
-if [ "$1" -ne "7.2" ] && [ "$1" -ne "7.3" ] && [ "$1" -ne "7.4" ]; then
+PHP_VER=`echo $1 | sed -e 's/\.//g'`
+if [ "$PHP_VER" -lt "72" ]; then
 	$minimal_apt_get_install php$1-mcrypt php-sodium
 fi
 
